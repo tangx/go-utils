f1257ee3eb598b649e8bbde42bae39cf5730cc0c
tangx <uyinn@live.com>
2021-02-18 14:55:07 +0800

    feat: add filex: dir walk

---
+++ filex/dirx.go
@@ -0,0 +1,40 @@
+package filex
+
+import (
+	"os"
+	"path/filepath"
+
+	"github.com/sirupsen/logrus"
+)
+
+func Walkdir(dirpath string, recursive bool) ([]string, error) {
+	fsList, err := os.ReadDir(dirpath)
+	if err != nil {
+		return nil, err
+	}
+
+	zipfiles := []string{}
+	for _, fs := range fsList {
+		subpath := filepath.Join(dirpath, fs.Name())
+
+		if fs.IsDir() {
+			logrus.Debugf("walking dir: %s", subpath)
+
+			if recursive {
+				files, err := Walkdir(subpath, true)
+				if err != nil {
+					logrus.Fatal(err)
+				}
+
+				zipfiles = append(zipfiles, files...)
+			}
+
+			continue
+		}
+
+		logrus.Debugf("walking file: %s", subpath)
+		zipfiles = append(zipfiles, filepath.Join(dirpath, fs.Name()))
+	}
+
+	return zipfiles, nil
+}
